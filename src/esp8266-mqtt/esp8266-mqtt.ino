a7ab33db40fc69bd06c29f72cf4f76ceac7f65fd
Pedro Padilha <pedropadilha@me.com>
2018-10-25 10:23:51 -0300

    Changes to sketch, digitalWrite working

---
+++ src/esp8266-mqtt/esp8266-mqtt.ino
@@ -0,0 +1,139 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* mqttServer = "test.mosquitto.org";
+const int mqttPort = 1883;
+
+const char* cred[] = {"Pedro", "3331926740"};
+
+const char* client_id = "WebLight";
+const char* thread = "pedropadilha13/trabalho/#";
+const char* response_topic = "pedropadilha13/trabalho/responses";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+
+  Serial.begin(115200);
+
+  wifi_setup();
+
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+
+  connect();
+
+  client.subscribe(thread);
+
+}
+
+void wifi_setup() {
+  WiFi.begin(cred[0], cred[1]);
+  Serial.println("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Success!");
+  Serial.print("IP Address is: ");
+  Serial.println(WiFi.localIP());
+}
+
+void connect() {
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect(client_id)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+
+  Serial.print("Message: ");
+  String message = "";
+
+  for (int i = 0; i < length; i++) {
+    message += (char) payload[i];
+  }
+
+  Serial.println(message);
+
+
+  String command = "";
+  String pin = "";
+  boolean toCommand = true;
+  for (int i = 24; i < strlen(topic); i++) {
+    if (topic[i] == 47) {
+      toCommand = false;
+    } else {
+      if (toCommand) {
+        command += (char) topic[i];
+      } else {
+        pin += (char) topic[i];
+      }
+    }
+  }
+
+  if (pin.equals("")) {
+    pin = message;
+  }
+
+  Serial.print("Command: ");
+  Serial.println(command);
+  Serial.print("pin: ");
+  Serial.println(pin);
+  Serial.print("Message: ");
+  Serial.println(message.toInt());
+
+  int pinNumber = pin.toInt();
+
+  if (command.equals("read") || command.equals("r") || command.equals("digitalRead") || command.equals("readDigital")) {
+    //Serial.print("digitalRead");
+    //Serial.println(pin.toInt());
+    pinMode(pinNumber, INPUT);
+    int reading = digitalRead(pinNumber);
+    char* response;
+    itoa(reading, response, 10);
+    client.publish(response_topic, response);
+  } else if (command.equals("analogRead") || command.equals("readAnalog")) {
+    //Serial.print("analogRead");
+    //Serial.println(pinNumber);
+    pinMode(pinNumber, INPUT);
+    int reading = analogRead(pinNumber);
+    char* response;
+    itoa(reading, response, 10);
+    client.publish(response_topic, response);
+  } else if (command.equals("digitalWrite") || command.equals("writeDigital") || command.equals("write") || command.equals("w")) {
+    Serial.print("digitalWrite");
+    Serial.println(pinNumber);
+    pinMode(pinNumber, OUTPUT);
+    boolean state = (message == "1" || message == "HIGH" || message == "true" || message == "on");
+    digitalWrite(pinNumber, state);    
+  } else if (command.equals("analogWrite") || command.equals("writeAnalog")) {
+    //Serial.print("analogWrite");
+    //Serial.println(pinNumber);
+    pinMode(pinNumber, OUTPUT);
+    analogWrite(pinNumber, message.toInt());
+  }
+
+  Serial.println();
+  Serial.println("-----------------------");
+
+
+}
+
+void loop() {
+  if (!client.connected()) {
+    connect();
+  }
+  client.loop();
+}
