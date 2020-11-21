229df2c0fb85002400afec6cc5bcbc8f09bb4699
Pedro Padilha <pedropadilha@me.com>
2020-11-21 00:25:43 -0300

    Changes before format

--- esp8266-mqtt/esp8266-mqtt.ino
+++ esp8266-mqtt/esp8266-mqtt.ino
@@ -4,9 +4,9 @@
 const char* mqttServer = "test.mosquitto.org";
 const int mqttPort = 1883;
 
-const char* cred[] = {"Pedro", "3331926740"};
+const char* cred[] = {"Sai Fora", "Colegao5449"};
 
-const char* client_id = "WebLight";
+const char* client_id = "StreamLight";
 const char* thread = "pedropadilha13/weblight/#";
 const char* response_topic = "pedropadilha13/weblight/responses";
 
